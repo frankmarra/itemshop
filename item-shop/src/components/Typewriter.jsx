340e8b7d37512c5057bd81a5eaab00b82bea898d
Frank Marra <frankrmarra@gmail.com>
2022-09-19 01:11:19 -0400

    cleaned up code and added some commments.

--- item-shop/src/components/Typewriter.jsx
+++ item-shop/src/components/Typewriter.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react'
 
+// This function creates the typewriter effect that is seen on the page description.  It takes two arguments.
+//  pageDescription is an array of letters that make up the description of the current page a user is on.
+//  placeHolder is a string of the pageDescription array that is used for display purposes.
 export default function Typewriter({ pageDescription, placeHolder }) {
   const [description, setDescription] = useState([])
 
+  // Call the function once, only on page load.
   useEffect(() => {
     typewriter()
   }, [])
+
+  // Takes the  array of letters pageDescription, takes the first letter of the array, and adds it to an array called description.  It then adds a timeout of 40ms before recalling itself until all letters from pageDescription are in the new description array.
   const typewriter = () => {
     if (pageDescription.length > 0) {
       let nextLetter = pageDescription.shift()
@@ -15,6 +21,8 @@ export default function Typewriter({ pageDescription, placeHolder }) {
     }
   }
 
+  // returns two paragraphs of text.  The first one is the place holder.  This defines the size of the div on screen.
+  // The second paragraph is what is actually displayed.  It appears letter by letter until the entire page description has been displayed.
   return (
     <div style={{ position: 'relative' }}>
       <p
